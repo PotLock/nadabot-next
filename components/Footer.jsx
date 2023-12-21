9bcd3ada3a85f0eeac2f3fee326115d2c10c78ef
M-RB3 <rabea9220@gmail.com>
2023-12-21 22:43:49 +0200

    update near footer icon

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,6 +1,15 @@
 import React from "react";
 import { links, socialLinks } from "./animations/data/footerLinks";
 
+const changeHover = (className) => {
+  console.log(className);
+  //   if (className.includes("random")) {
+  //     className.remove("random");
+  //   } else {
+  //     className.add("random");
+  //   }
+};
+
 const Footer = () => {
   return (
     <div className="bg-footer-texture bg-[#FAFAFA] w-full pt-32 flex justify-center">
@@ -10,8 +19,15 @@ const Footer = () => {
         </div>
         <div className="text-[#2D6FDB] font-oi text-7xl mt-6">nada.bot</div>
         <div className="flex font-medium justify-between max-w-[701px] w-full mt-12">
-          {links.map((link) => (
-            <a href={link.url} key={link.title}>
+          {links.map((link, idx) => (
+            <a
+              href={link.url}
+              className={`footer-link transition-all ${
+                idx % 2 === 0 ? "random" : ""
+              }`}
+              target="_blank"
+              key={link.title}
+            >
               {link.title}
             </a>
           ))}
@@ -19,8 +35,14 @@ const Footer = () => {
         <div className="w-full h-[1px] bg-[#292929] mt-14 mb-8" />
         <div className="flex gap-3 mb-8">
           {socialLinks.map((link) => (
-            <a href={link.url} key={link.url}>
-              <img src={link.icon} alt={link.url} />
+            <a
+              href={link.url}
+              className="footer-social-links"
+              target="_blank"
+              key={link.url}
+            >
+              {/* <img src={link.icon} alt={link.url} /> */}
+              {link.icon}
             </a>
           ))}
         </div>
--- components/animations/data/footerLinks.js
+++ components/animations/data/footerLinks.js
@@ -1,3 +1,8 @@
+import Twitter from "../../../public/assets/footer/twitter.svg";
+import Linkedin from "../../../public/assets/footer/linkedin.svg";
+import Telegram from "../../../public/assets/footer/telegram.svg";
+import Near from "../../../public/assets/footer/near.svg";
+
 export const links = [
   {
     title: "Docs",
@@ -22,19 +27,37 @@ export const links = [
 ];
 export const socialLinks = [
   {
-    icon: "/assets/footer/twitter.svg",
+    icon: <Twitter />,
     url: "/twitter",
   },
   {
-    icon: "/assets/footer/linkedin.svg",
+    icon: <Linkedin />,
     url: "/linkedin",
   },
   {
-    icon: "/assets/footer/telegram.svg",
+    icon: <Telegram />,
     url: "/telegram",
   },
   {
-    icon: "/assets/footer/near.svg",
+    icon: <Near />,
     url: "/near",
   },
 ];
+// export const socialLinks = [
+//   {
+//     icon: "/assets/footer/twitter.svg",
+//     url: "/twitter",
+//   },
+//   {
+//     icon: "/assets/footer/linkedin.svg",
+//     url: "/linkedin",
+//   },
+//   {
+//     icon: "/assets/footer/telegram.svg",
+//     url: "/telegram",
+//   },
+//   {
+//     icon: "/assets/footer/near.svg",
+//     url: "/near",
+//   },
+// ];
