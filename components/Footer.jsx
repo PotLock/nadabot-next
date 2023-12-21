5fefd158ac37760bef1232cf7670c873d68760a2
M-RB3 <rabea9220@gmail.com>
2023-12-21 21:11:22 +0200

    project cleanup

---
+++ components/Footer.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { links, socialLinks } from "./animations/data/footerLinks";
+
+const Footer = () => {
+  return (
+    <div className="bg-footer-texture bg-[#FAFAFA] w-full pt-32">
+      <div className="contaienr flex flex-col items-center">
+        <div className="text-[#F7CE55] text-6xl font-medium stroke-black footer-title">
+          prove that you are
+        </div>
+        <div className="text-[#2D6FDB] font-oi text-7xl mt-6">nada.bot</div>
+        <div className="flex font-medium justify-between max-w-[701px] w-full mt-12">
+          {links.map((link) => (
+            <a href={link.url} key={link.title}>
+              {link.title}
+            </a>
+          ))}
+        </div>
+
+        <div className="flex gap-3">
+          {socialLinks.map((link) => (
+            <a href={link.url} key={link.url}>
+              <img src={link.icon} alt={link.url} />
+            </a>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
