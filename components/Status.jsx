165b7d3cc621e6ab61047427bac5cf251ee7417f
M-RB3 <rabea9220@gmail.com>
2024-01-04 09:39:08 +0200

    update /app route

--- components/Status.jsx
+++ components/Status.jsx
@@ -2,17 +2,17 @@ import React from "react";
 
 const Status = () => {
   return (
-    <div className="bg-[#F0F0F0] flex justify-center w-full py-6 mt-14 px-10 max-md:px-5">
+    <div className="mt-14 flex w-full justify-center bg-[#F0F0F0] px-10 py-10 max-md:px-5">
       <div className="container grid grid-cols-3 max-md:grid-cols-1">
-        <div className="flex flex-col items-center text-[#292929] gap-3 max-md:py-4">
+        <div className="flex flex-col items-center gap-3 text-[#292929] max-md:py-4">
           <div className="text-7xl font-extrabold">0</div>
           <div className="text-2xl font-medium">Humans Verified</div>
         </div>
-        <div className="flex flex-col items-center text-[#292929] border-x border-[#A6A6A6] max-md:py-4 max-md:border-y max-md:border-x-0 gap-3">
+        <div className="flex flex-col items-center gap-3 border-x border-[#A6A6A6] text-[#292929] max-md:border-x-0 max-md:border-y max-md:py-4">
           <div className="text-7xl font-extrabold">2</div>
           <div className="text-2xl font-medium">Verified Providers</div>
         </div>
-        <div className="flex flex-col items-center text-[#292929] gap-3 max-md:py-4">
+        <div className="flex flex-col items-center gap-3 text-[#292929] max-md:py-4">
           <div className="text-7xl font-extrabold">0</div>
           <div className="text-2xl font-medium">Total Checks</div>
         </div>
--- pages/[redirect].js
+++ pages/[redirect].js
@@ -9,7 +9,7 @@ const RedirectLinks = {
   "/support": "https://t.me/+eBl5J1sCHaU3ODVh",
   "/potlock":
     "https://near.social/potlock.near/widget/Index?tab=project&projectId=nadabot.near",
-  "/app": "https://near.social/potlock.near/widget/Index",
+  "/app": "https://app.nada.bot/",
   "/docs": "https://docs.potlock.io/contracts/sybil-contract-wip",
   "/contract": "https://github.com/PotLock/core/tree/main/contracts/sybil",
   "/near": "https://near.social/mob.near/widget/MyPage?accountId=nadabot.near",
