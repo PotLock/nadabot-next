1f80b1e8976d29f5fb58048b1a74797b0f33c929
M-RB3 <rabea9220@gmail.com>
2024-01-04 09:34:36 +0200

    config ga

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import "./style.css";
+import { GoogleAnalytics } from "nextjs-google-analytics";
 
 import React from "react";
 export default function MyApp({ Component: Component, pageProps: pageProps }) {
@@ -10,6 +11,7 @@ export default function MyApp({ Component: Component, pageProps: pageProps }) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <Component {...pageProps} />
+      <GoogleAnalytics trackPageViews gaMeasurementId="G-KNW8YJS0Q5" />
     </>
   );
 }
